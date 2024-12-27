1bd9c6f1ffdcd3c4a5b91c1e001a9e43e80874bc
JoPad <100805221+JoPadOfficiel@users.noreply.github.com>
2024-12-27 10:29:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+//script.js - code pour charger la biographie
+fetch('../biographie.txt')
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('biographie').querySelector('p').textContent = text;
+    });
